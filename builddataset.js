571df9053d1e815619ef80d1704d241c4fc8805d
samsuanchen <samsuanchen@gmail.com>
2015-10-23 19:02:38 +0800

    glyph.js and glyphs.js done

--- builddataset.js
+++ builddataset.js
@@ -1,12 +1,16 @@
+console.log('loading builddataset');
 var fs=require('fs');
+var bsearch=require("./bsearch");
+var ucs2string=require("./uniutil").ucs2string;
+var ucs=function(c){
+	if(c)
+		return c.match(/^u/)?ucs2string(parseInt(c.substr(1),16)):c;
+}
 var keys={}, gdata=[];
 
 var getGlyph=function(line){
 	var fields=line.split(/\s+\|\s+/);
-	var name=fields[0].trim();
-	var related=fields[1];
-	var data=fields[2].trim();
-	return {name:name, related:related, data:data}
+	return {name:fields[0].trim(), data:fields[2].trim()}
 }
 var d3=function(n){
 	n=n.toString();
@@ -15,29 +19,159 @@ var d3=function(n){
 var getData=function(key){
 	//console.log(key)
 	var i=keys[key];
-	console.log('keys["'+key+'"]='+i);
-	console.log('gdata['+i+']="'+gdata[i]+'"');
+//	console.log('keys["'+key+'"]='+i);
+//	console.log('gdata['+i+']="'+gdata[i]+'"');
 	if(i!==undefined)
 		return gdata[i];
 }
-
-var builddataset=function(){
+var file, text, nChars, line, nLines;
+file='../dump_newest_only.txt';
+console.log('reading '+file)
+text=fs.readFileSync(file,'utf8').replace(/@\d+/g,''); ///// unrelated 115858, 59093200-58977342
+nChars=text.length;
+console.log(nChars+' characters read as input text');
+line=text.split(/\r?\n/);
+nLines=line.length;
+console.log('split text into '+nLines+' lines as line[0] to line['+(nLines-1)+']');
+var buildDataset=function(){
 	var i1=2,j=0;
 	var i9=407415;
-	var lines=fs.readFileSync('dump_newest_only.txt','utf8').split(/\r?\n/);
 	for(var i=i1; i<=i9; i++){
-		var g=getGlyph(lines[i]);
+		var L=line[i];
+		if(L.match(/undefined$/)) continue;
+		var g=getGlyph(L);
 		gdata[j]=g.data;
 		keys[g.name]=j++;
 		if(j%1000===0){
 			var k=Math.floor((j-1)/1000), f='dataset'+d3(k)+'.txt';
 			k=k*1000;
-			console.log(f+' '+k+'~'+j+' gdata written');
+		//	if(j<10000)
+		//		console.log('writing '+f+' '+k+'~'+j+' gdata written');
+		//	fs.writeFileSync(f,gdata.slice(k,j).join('\n'))
 		}
 	}
 	var k=Math.floor((j-1)/1000), f='dataset'+d3(k)+'.txt';
 	k=k*1000;
-	console.log(f+' '+k+'~'+j+' gdata written');
-	fs.writeFileSync(f,gdata.slice(k,j).join('\n'));	
+//	console.log('writing '+f+' '+k+'~'+j+' gdata written');
+//	fs.writeFileSync(f,gdata.slice(k,j).join('\n'));
 }
-module.exports={build:builddataset,getData:getData,gdata:gdata,getGlyph:getGlyph,keys:keys};
+var theComponents={},nComponents=0;
+var getComponents=function(unicode){
+//	console.log('getComponents("'+unicode+'")');
+	var cc=theComponents[unicode];
+//	console.log('cc=theComponents["'+unicode+'"]='+cc);
+	if(cc)
+		return cc;
+	var d=getData(unicode);
+//	console.log('d=getData("'+unicode+'")="'+d+'"');
+	cc=d.match(/:([a-z][^:$]+)(:|\$|$)/g);
+//	console.log('cc=d.match(/:(\D+)(\$|$)/g)='+cc);
+	if(cc) {
+		cc=cc.map(function(c){
+			var n=c.match(/[^:$]+/)[0];
+			return n;
+		});
+		theComponents[unicode]=cc;
+		nComponents++;
+	}
+	return cc;
+}
+var partsUsedFr={};
+var partsSorted=[];
+var nonUnicodeParts=[];
+var unicodesOnly=[];
+var pUnicode=/^u[0-9a-f]{4,5}$/;
+var checkNonUnicodeParts=function(unicode){
+	var cc=getComponents(unicode);
+	if(!cc) return
+	var nu=[];
+	cc.forEach(function(c){
+		if(!c)return;
+		var fr=partsUsedFr[c]||0
+		partsUsedFr[c]=fr;
+		if(!fr&&!c.match(pUnicode)){
+			// non Unicode Part first used
+			nonUnicodeParts.push(c);
+		}
+		partsUsedFr[c]++;
+	})
+}
+var analyzeUnicodeOnly=function(){
+	var names=Object.keys(keys);
+	names.forEach(function(name){
+		if(name.match(pUnicode)) {
+			var code=name.substr(1), u=parseInt(code,16);
+			if(u>=0x3400&&u<0x9fe9||u>=0x20000&&u<=0x2cea1){
+				// 包括 ExtA u3400~u4dbf, ExtB u20000~u2A6D6, ExtC u2A700~u2B734, ...
+				unicodesOnly.push((u<0x10000?'0':'')+code);
+				checkNonUnicodeParts(name);
+			}
+		}
+	})
+	console.log('Unicode Only 筆數: '+unicodesOnly.length);
+	fs.writeFileSync('unicodeOnly.txt', unicodesOnly.sort().join(' '));
+	console.log('nonUnicode Parts 筆數: '+nonUnicodeParts.length);
+	fs.writeFileSync('nonUnicodeParts.txt', nonUnicodeParts.join(' '));
+	var kk=Object.keys(theComponents);
+	var cc=kk.map(function(u){
+		return u+' '+ucs(u)+' <== '+theComponents[u].map(function(u){
+			return u+' '+ucs(u);
+		}).join(' ');
+	})
+	console.log('components 筆數: '+Object.keys(theComponents).length);
+	fs.writeFileSync('components.txt', cc.join('\r\n'));
+	var list=Object.keys(partsUsedFr);
+	console.log('partsUsedFr list: '+list.length);
+	partsSorted=list.sort(function(a,b){
+		return partsUsedFr[b]-partsUsedFr[a]; // 從大到小排序
+	})
+	list=partsSorted.map(function(u){
+		return u+' '+ucs(u)+' '+partsUsedFr[u];
+	})
+	fs.writeFileSync('partsSortedFr.txt', list.join('\r\n'));
+}
+var glyphs=[], glyph={}, gNames=[];
+var buildAllGlyphs=function(){
+	gNames=unicodesOnly.concat(nonUnicodeParts);
+	console.log('gNames',gNames.length);
+	gNames=gNames.map(function(g,i){
+		if(i<80459){
+			g=g.replace(/^0?/,function(m0){
+				return 'u';
+			})
+		}
+		glyph[g]=i;
+		glyphs[i]=getData(g);
+		return g;
+	})
+	fs.writeFileSync('gNames.js','module.exports='+JSON.stringify(gNames));
+	fs.writeFileSync('glyphs.js','module.exports='+JSON.stringify(glyphs));
+	fs.writeFileSync('glyph.js','module.exports='+JSON.stringify(glyph));
+}
+console.log('buildDataset');
+buildDataset();
+console.log('analyzeUnicodeOnly');
+analyzeUnicodeOnly();
+console.log('buildAllGlyphs');
+buildAllGlyphs();
+module.exports={
+	file: file,
+	text: text,
+	nChars: nChars,
+	line: line,
+	nLines: nLines,
+	gdata: gdata,
+	keys: keys,
+	partsUsedFr: partsUsedFr,
+	partsSorted: partsSorted,
+	unicodesOnly: unicodesOnly,
+	nonUnicodeParts: nonUnicodeParts,
+	theComponents: theComponents,
+	gNames: gNames,
+	glyphs: glyphs,
+	glyph: glyph,
+	ucs: ucs,
+	getData: getData,
+	getGlyph: getGlyph,
+	getComponents: getComponents
+};
--- test/testbuilddataset.js
+++ test/testbuilddataset.js
@@ -12,24 +12,23 @@ console.log('D=require("../builddataset")');
 var D=require("../builddataset");
 
 describe("10 原始檔案 "+D.file,function(){
-	it("11 原始字數 59093200",function(){
-		assert.deepEqual(D.nChars,59093200);
+	it("11 原始字數 58977342",function(){
+		assert.deepEqual(D.nChars,58977342);
 	});
 	it("12 原始列數 407419",function(){
 		assert.deepEqual(D.nLines,407419);
 	});
 	var i1=2;
 	it("13 首筆資料 abcabc_sandbox",function(){
-		var g=D.getGlyph(D.line[i1]);	
+		var g=D.getGlyph(D.line[i1]);
+
 		assert.deepEqual(g.name,"abcabc_sandbox");
-		assert.deepEqual(g.related,"u3013");
 		assert.deepEqual(g.data,"99:0:0:0:0:163:200:u571f-01:0:0:0$99:0:0:58:0:200:200:u757f:0:0:0");
 	});
 	var i9=407415;
 	it("14 末筆資料 zzzfelis_livermorium",function(){
 		var g=D.getGlyph(D.line[i9]);	
 		assert.deepEqual(g.name,"zzzfelis_livermorium");
-		assert.deepEqual(g.related,"u3013");
 		assert.deepEqual(g.data,"1:0:32:138:18:138:57$1:0:0:96:57:179:57$2:7:8:106:72:119:104:123:159$2:0:7:163:74:156:126:144:174$1:0:0:90:174:186:174$2:0:7:58:16:44:59:16:93$2:7:8:57:28:79:39:88:55$1:0:0:37:72:87:72$1:0:0:22:104:92:104$1:32:32:57:72:57:165$2:7:8:24:121:37:135:39:155$2:0:7:83:120:77:136:68:152$2:0:7:20:178:56:170:90:158");
 	});
 });
@@ -62,8 +61,8 @@ describe("30 Unicode 分析",function(){
 	it("34 部件頻次 partsUsedFr u8279-03 "+ucs("u8279-03")+" 3046",function(){
 		assert.deepEqual(D.partsUsedFr['u8279-03'],3046);
 	});
-	it("35 所有部件 partsUsedFr 筆數 21476",function(){
-		assert.deepEqual(Object.keys(D.partsUsedFr).length,21476);
+	it("35 所有部件 partsUsedFr 筆數 20501",function(){
+		assert.deepEqual(Object.keys(D.partsUsedFr).length,20501);
 	});
 	it("36 部件排序 partsSorted 首5筆 u8279-03 艹 u6c35-01 氵 u53e3-01 口 u6728-01 木 u624c-01 扌",function(){
 		var s=D.partsSorted.slice(0,5).map(function(u){
@@ -71,13 +70,26 @@ describe("30 Unicode 分析",function(){
 		}).join(' ');
 		assert.deepEqual(s,"u8279-03 艹 u6c35-01 氵 u53e3-01 口 u6728-01 木 u624c-01 扌");
 	});
-	it("37 外加部件 nonUnicodeParts 筆數 16766",function(){
-		assert.deepEqual(D.nonUnicodeParts.length,16766);
+	it("37 外加部件 nonUnicodeParts 筆數 13698",function(){
+		assert.deepEqual(D.nonUnicodeParts.length,13698);
 	});
 })
-
-describe("40 Unicode 擷取",function(){
-	it("41 nonUnicodeParts 筆數 16766",function(){
-		assert.deepEqual(D.nonUnicodeParts.length,16766);
+describe("40 gNames, glyphs, and glyph",function(){
+	it("41 gNames 筆數 94157",function(){
+		console.log('gNames',typeof D.gNames);
+		assert.deepEqual(D.gNames.length,94157);
+	});
+	it("42 glyph['u3400']===0 to get glyphs[0] as data",function(){
+		assert.deepEqual(D.glyph['u3400'],0);
+		assert.deepEqual(D.glyphs[0],'1:0:32:72:18:72:178$1:0:32:119:18:119:178$1:2:2:22:86:72:86$1:2:0:119:86:174:86$1:0:0:19:178:182:178');
+		assert.deepEqual(D.glyph['u20003-jv'],80459);
+		assert.deepEqual(D.glyphs[80459],'1:12:32:55:24:55:179$1:2:2:55:24:144:24$1:22:32:144:24:144:179$1:0:0:19:179:182:179');
 	});
 })
+/*
+describe("50 取常用字",function(){
+	it("51 取常用字 筆數 4820",function(){
+		assert.deepEqual(D.常用字.length,4820);
+	});
+})
+*/
